134ea51cb5a2de9581ab00b12bcd3dcb5cd15434
Shashank-09 <shashanktulsikar979@gmail.com>
2024-01-19 21:06:11 +0530

    fifth commit

---
+++ 05reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const Github = () => {
+    const [data , setData] = useState([]);
+    useEffect(() => {
+       fetch('https://api.github.com/users/Shashank-09')
+       .then(response => response.json())
+       .then(data => {
+          setData(data)
+       })
+    } , [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl '>Github Followers: {data.followers} </div>
+  )
+}
+
+export default Github
---
+++ 05reactRouter/src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter , Route , createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import ContactUs from './components/Contact/ContactUs.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
+
+
+//Method 1st for routing
+// const router = createBrowserRouter ([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path:"contact",
+//         element: <ContactUs />
+//       }
+      
+//     ]
+//   }
+// ])
+
+//  //Method 2nd for routing
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}> 
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<ContactUs />} />
+      <Route path='github' element={<Github />} />
+      <Route path='user/:userid' element={<User />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
