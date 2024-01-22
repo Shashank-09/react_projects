9bbd46939bbd2f833ea9d39ac42fa83b494eddbe
Shashank-09 <shashanktulsikar979@gmail.com>
2024-01-22 23:51:38 +0530

    router updated

--- 05reactRouter/src/main.jsx
+++ 05reactRouter/src/main.jsx
@@ -12,43 +12,43 @@ import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 
 //Method 1st for routing
-// const router = createBrowserRouter ([
-//   {
-//     path: '/',
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "",
-//         element: <Home />
-//       },
-//       {
-//         path: "about",
-//         element: <About />
-//       },
-//       {
-//         path:"contact",
-//         element: <ContactUs />
-//       }
+const router = createBrowserRouter ([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path:"contact",
+        element: <ContactUs />
+      }
       
-//     ]
-//   }
-// ])
+    ]
+  }
+])
 
 //  //Method 2nd for routing
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}> 
-      <Route path='' element={<Home />}/>
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<ContactUs />} />
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />} />
-      <Route path='user/:userid' element={<User />} />
-    </Route>
-  )
-)
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}> 
+//       <Route path='' element={<Home />}/>
+//       <Route path='about' element={<About />} />
+//       <Route path='contact' element={<ContactUs />} />
+//       <Route 
+//       loader={githubInfoLoader}
+//       path='github' 
+//       element={<Github />} />
+//       <Route path='user/:userid' element={<User />} />
+//     </Route>
+//   )
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
